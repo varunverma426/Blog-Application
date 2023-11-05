9c53611fab5eca46d03ec1299ab0fa47895b600e
varunverma426 <60269347+varunverma426@users.noreply.github.com>
2023-11-06 00:56:14 +0530

    Categories Api
    Categories Api

---
+++ src/main/java/com/blogapplication/Services/CategoryServices.java
@@ -0,0 +1,19 @@
+package com.blogapplication.Services;
+
+import com.blogapplication.payloads.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryServices {
+
+    //create
+     CategoryDTO createCategory(CategoryDTO categoryDto);
+    //update
+     CategoryDTO updateCategory(CategoryDTO categoryDto,Integer categoryId);
+    //delete
+    void deleteCategory(Integer categoryId);
+    //getAll
+    List<CategoryDTO> getAllCategory();
+    //getById
+    CategoryDTO getCategoryById(Integer categoryId);
+}
